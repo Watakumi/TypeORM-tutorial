591c41544826cf4ea818b2fdd629df773197c891
石渡拓海 <t-ishiwata@sight-visit.com>
2021-12-31 16:34:51 +0900

    Usersモジュールから読み込む

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), UsersModule],
-  controllers: [AppController, UsersController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
