f625c40e1c0313627cbf3d0c28836d872aaff51a
石渡拓海 <t-ishiwata@sight-visit.com>
2021-12-31 16:29:15 +0900

    nest g module users

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(), UsersModule],
   controllers: [AppController, UsersController],
   providers: [AppService],
 })
