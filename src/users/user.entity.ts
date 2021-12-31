fd4f8b0f72bf5bdddcfa3c6e1c670a1e48f52f42
石渡拓海 <t-ishiwata@sight-visit.com>
2021-12-31 16:25:19 +0900

    Userエンティティの追加

---
+++ src/users/user.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('int')
+  age: number;
+
+  @Column('varchar')
+  firstName: string;
+
+  @Column('varchar')
+  lastName;
+}
