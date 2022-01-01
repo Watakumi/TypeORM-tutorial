91c83dd16154c0f9a55d5445475c4a4d8fdd9f65
石渡拓海 <t-ishiwata@sight-visit.com>
2022-01-01 15:22:11 +0900

    Serviceを読み込む

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/user.entity';
 import { UsersModule } from 'src/users/users.module';
 import { Post } from './post.entity';
 import { PostsController } from './posts.controller';
+import { PostsService } from './posts.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post]), UsersModule],
-  providers: [],
+  imports: [TypeOrmModule.forFeature([Post, User]), UsersModule],
   controllers: [PostsController],
+  providers: [PostsService],
 })
 export class PostsModule {}
